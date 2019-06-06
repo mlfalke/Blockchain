566824d43dc40703297d043faab407e1d3c8fc4e
matthijs <matthijsfalke@hotmail.com>
2019-06-06 12:28:25 +0200

    update encryptie

--- Program.cs
+++ Program.cs
@@ -2,31 +2,69 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+
+using System.Security.Permissions;
 using System.Text;
 using Newtonsoft.Json;
+
+
+
+
 
 namespace Blockchain
 {
     class Program
     {
+
+        
+
         static void Main(string[] args)
         {
-            
-            Blockchain GovernmentChain = new Blockchain();
-            FileStream filestreamCreate = new FileStream("Blockchain.txt", FileMode.OpenOrCreate);
+            string Private = @"private_key.pem";
+            string Public = @"public_key.pem";
+            Key.GenerateRsaKeyPair(Private, Public);
 
-            GovernmentChain.AddBlock(new Block(DateTime.Now,null,  "{sender:deze}"));
+            //;
+            //Encryption.AddToMyStore(Encryption.Create("watdan", DateTime.Now, DateTime.MaxValue, "Hetwachtwoord"));
+            Blockchain GovernmentChain = new Blockchain();
+            //FileStream filestreamCreate = new FileStream("Blockchain.txt", FileMode.OpenOrCreate);
+             // The path to the certificate.
+            //string Certificate =  @"0AE76E4CB07854E7CE19866BB97CE73246DFAA27.cer";
+            
+			//Create X509Certificate2 object from .cer file.
+            //X509Certificate2 x509 = new X509Certificate2(File.ReadAllBytes(Certificate));
+			//byte[] rawData = Encryption.ReadFile(Certificate);
+            
+
+			
+    	    // Console.WriteLine("{0}Public Key Format: {1}{0}",Environment.NewLine,x509.PublicKey.Key.ToString());
+            // Console.WriteLine("{0}Certificate to XML String: {1}{0}",Environment.NewLine,x509.PublicKey.ToString());
+            //Load the certificate into an X509Certificate object.
+            // X509Certificate cert = X509Certificate.CreateFromCertFile(Certificate);
+    	    //Encryption.PrivateKeyFromPemFile(Certificate);
+            // // //Get the value.
+            // byte[] results = cert.GetPublicKey();
+            // Encryption.IsInMyStore();
+            // // Display the value to the console.
+            // foreach(byte b in results)
+            // {
+            //     Console.Write(b);
+            // }
+            //Reads a file.
+            
+            GovernmentChain.AddBlock(new Block(DateTime.Now,null,"{sender:deze}"));
 
             Console.WriteLine(JsonConvert.SerializeObject(GovernmentChain, Formatting.Indented));
-
-            
-
-            using(StreamWriter writer = new StreamWriter(filestreamCreate))
-            {
+            //string fileName =  @"397B767444CA06357BE84C8F0D914C3C9A64C4EF.cer";
+           
+			
+            // using(StreamWriter writer = new StreamWriter(filestreamCreate))
+            // {
                 
-                writer.Write(JsonConvert.SerializeObject(GovernmentChain, Formatting.Indented));
+            //     writer.Write(JsonConvert.SerializeObject(GovernmentChain, Formatting.Indented));
                 
-            };
+            // };
             
             
             Console.ReadKey();
--- lib/Block.cs
+++ lib/Block.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 
 namespace Blockchain
@@ -11,28 +12,26 @@ namespace Blockchain
     public string PreviousHash { get; set; }  
     public string Hash { get; set; }  
     public string Data { get; set; }  
-    public string Ddata { get; set; }
+    
+
   
     public Block(DateTime timeStamp, string previousHash, string data)  
     {  
-        //assymetric encryption method
-        //name of keycontainer
-        // string KeyContainerName = "MyKeyContainer";
-        // Key.RSAPersistKeyInCSP(KeyContainerName);
-        // UnicodeEncoding ByteConverter = new UnicodeEncoding();
-        //encrypt string
-        // byte[] Bdata = Encryption.RSAEncrypt(ByteConverter.GetBytes(data),KeyContainerName,false);
-        //decrypt bytes
-        // byte[] Rdata = Encryption.RSADecrypt(Bdata,KeyContainerName,false);
-
-        string Sdata = RSACryptography.CryptographyHelper.Encrypt(data);
-        string Ddata= RSACryptography.CryptographyHelper.Decrypt(Sdata); 
-
+        // assymetric encryption method
+        // name of keycontainer
+        //FUCKDEZECODE(data);
+        // decrypt bytes
+        //byte[] Rdata = Encryption.RSADecrypt(Bdata,KeyContainerName,false);
+        
+        
+        //string Sdata = RSACryptography.CryptographyHelper.Encrypt(data);
+        //string Ddata= RSACryptography.CryptographyHelper.Decrypt(Sdata); 
+        
 
         Index = 0;  
         TimeStamp = timeStamp;  
         PreviousHash = previousHash;  
-        Data = Ddata;
+        Data = FUCKDEZECODE(data);
         Hash = CalculateHash();  
     }  
 
@@ -46,6 +45,31 @@ namespace Blockchain
         byte[] outputBytes = sha256.ComputeHash(inputBytes);  
   
         return Convert.ToBase64String(outputBytes);  
-    }  
+    } 
+    public string FUCKDEZECODE(string data)
+    {
+
+        string Private = "private_key.pem";
+        string Public = "public_key.pem";
+        
+        UnicodeEncoding ByteConverter = new UnicodeEncoding();
+        var ndata = ByteConverter.GetBytes(data);
+        string output = string.Empty;
+        X509Certificate2 cert = Encryption.Create("watdan", DateTime.Now, DateTime.MaxValue, "Hetwachtwoord");
+        using (RSA rsa = cert.GetRSAPublicKey())
+        {
+            return Convert.ToBase64String(rsa.Encrypt(ndata, RSAEncryptionPadding.OaepSHA1));
+        }
+
+       
+
+
+        // var Sdata = Encryption.RSAEncrypt(ByteConverter.GetBytes(data),"key",false);
+        // var RData = Encryption.RSADecrypt(Sdata,"key",false);
+
+        
+    }
+
 }
+
 }
